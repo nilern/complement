6dbf903e94364cf87d9b0e51f5d5a6819b0bd1d6
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-08-31 10:29:37 +0300

    Initial commit.

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection "complement")
+(define deps '("base" "nanopass"))
+(define pkg-desc "Transforming an interpreter into a compiler")
+(define version "0.0")
+(define pkg-authors '(nilern))
