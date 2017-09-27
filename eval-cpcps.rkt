6d0b80268c2e2cd51e01f2577116e22050e8c412
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-09-27 18:21:19 +0300

    Keep track of current function, not the current label.

--- eval-cpcps.rkt
+++ eval-cpcps.rkt
@@ -21,18 +21,19 @@
 ;; TODO: dominator scoping rule
 (define-pass eval-CPCPS : CPCPS (ir) -> * ()
   (definitions
-    (define (eval-block stmts transfer curr-label env kenv fenv rfenv)
+    (define (eval-block stmts transfer curr-fn env kenv fenv rfenv)
       (match stmts
-        [(cons stmt stmts*) (Stmt stmt curr-label env kenv fenv rfenv stmts* transfer)]
-        ['() (Transfer transfer curr-label env kenv fenv rfenv)]))
+        [(cons stmt stmts*) (Stmt stmt curr-fn env kenv fenv rfenv stmts* transfer)]
+        ['() (Transfer transfer curr-fn env kenv fenv rfenv)]))
 
-    (define (goto k curr-label env kenv fenv rfenv args)
+    (define (goto k curr-fn env kenv fenv rfenv args)
       (nanopass-case (CPCPS Cont) (kenv:ref kenv k)
         [(cont (,n* ...) ,s* ... ,t)
-         (let ([env (if (eq? k curr-label)
-                      (env:push-args env n* args)
-                      (for/hash ([name n*] [arg args]) (values name arg)))])
-           (eval-block s* t k env kenv fenv rfenv))]))
+         (define next-fn (kenv:ref rfenv k))
+         (define env* (if (eq? next-fn curr-fn)
+                        (env:push-args env n* args)
+                        (for/hash ([name n*] [arg args]) (values name arg))))
+         (eval-block s* t next-fn env* kenv fenv rfenv)]))
 
     (define (primapply fenv op args)
       (match* (op args)
@@ -53,7 +54,6 @@
 
   (Program : Program (ir) -> * ()
     [(prog ([,n1* ,f*] ...) ([,n2* ,k*] ...) ,n)
-     (define env (env:empty))
      ;; HACK:
      (define-values (kenv fenv rfenv)
        (for/fold ([kenv (kenv:inject n2* k*)]
@@ -67,32 +67,33 @@
                     (for/fold ([rfenv rfenv])
                               ([label n*])
                       (hash-set rfenv label name)))])))
-     (goto n n env kenv fenv rfenv '())])
+     (define env (env:empty))
+     (goto n #f env kenv fenv rfenv '())])
 
-  (Stmt : Stmt (ir curr-label env kenv fenv rfenv stmts transfer) -> * ()
-    [(def ,n ,e) (Expr e curr-label env kenv fenv rfenv n stmts transfer)]
-    [,e (Expr e curr-label env kenv fenv rfenv #f stmts transfer)])
+  (Stmt : Stmt (ir curr-fn env kenv fenv rfenv stmts transfer) -> * ()
+    [(def ,n ,e) (Expr e curr-fn env kenv fenv rfenv n stmts transfer)]
+    [,e (Expr e curr-fn env kenv fenv rfenv #f stmts transfer)])
 
-  (Expr : Expr (ir curr-label env kenv fenv rfenv name stmts transfer) -> * ()
+  (Expr : Expr (ir curr-fn env kenv fenv rfenv name stmts transfer) -> * ()
     [,a
      (define res (Atom a env kenv fenv))
      (define env* (if name (env:insert env name res) env))
-     (eval-block stmts transfer curr-label env* kenv fenv rfenv)]
+     (eval-block stmts transfer curr-fn env* kenv fenv rfenv)]
     [(primcall ,p ,a* ...)
      (define res (primapply fenv p (map (cute Atom <> env kenv fenv) a*)))
      (define env* (if name (env:insert env name res) env))
-     (eval-block stmts transfer curr-label env* kenv fenv rfenv)])
+     (eval-block stmts transfer curr-fn env* kenv fenv rfenv)])
 
-  (Transfer : Transfer (ir curr-label env kenv fenv rfenv) -> * ()
+  (Transfer : Transfer (ir curr-fn env kenv fenv rfenv) -> * ()
     [(goto ,x ,a* ...)
-     (goto (Var x env kenv fenv) curr-label env kenv fenv rfenv
+     (goto (Var x env kenv fenv) curr-fn env kenv fenv rfenv
            (map (cute Atom <> env kenv fenv) a*))]
     [(if ,a? (,x1 ,a1* ...) (,x2 ,a2* ...))
      (define-values (x a*)
        (match (Atom a? env kenv fenv)
          [#t (values x1 a1*)]
          [#f (values x2 a2*)]))
-     (goto (Var x env kenv fenv) curr-label env kenv fenv rfenv
+     (goto (Var x env kenv fenv) curr-fn env kenv fenv rfenv
            (map (cute Atom <> env kenv fenv) a*))]
     [(halt ,a) (Atom a env kenv fenv)])
 
