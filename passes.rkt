718d6bdb7a1e8c527f9c5d539b01c9f1a47916b1
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-10-03 19:42:17 +0300

    Compute dominator forests of CPCPS control flow graphs.

--- passes.rkt
+++ passes.rkt
@@ -659,6 +659,8 @@
   (require (only-in srfi/26 cute) nanopass/base
            "langs.rkt")
 
+  ;;; FIXME: callers and callees should disallow duplicates
+
   (define (make-entry)
     (make-hash '((escapes? . #f) (callers . ()) (callees . ()))))
 
@@ -733,6 +735,82 @@
   (for ([entry entries])
     (visit! entry))
   rpo)
+
+;; OPTIMIZE: use worklist algorithm
+(define (cpcps-cfg-dominator-forest label-table entries)
+  (define rpo (cpcps-cfg-rpo label-table entries))
+  (define label-rpo-indices (for/hash ([(label i) (in-indexed rpo)])
+                              (values label i)))
+  (define changed #t)
+
+  (define (olset= set1 set2)
+    (eq? (car set1) (car set2)))
+
+  (define (olset-intersect doms1 doms2)
+    (let ([idom1-idx (hash-ref label-rpo-indices (car doms1))]
+          [idom2-idx (hash-ref label-rpo-indices (car doms2))])
+      (cond
+        [(< idom1-idx idom2-idx) (olset-intersect (cdr doms1) doms2)]
+        [(> idom1-idx idom2-idx) (olset-intersect doms1 (cdr doms2))]
+        [else doms1])))
+
+  (define (intersect* dom-chain-hashes)
+    (for/fold ([dom-chain-hashes-acc (hash)])
+              ([entry entries])
+      (define dom-chain
+        (for/fold ([dom-chain-acc #f])
+                  ([dom-chain-hash dom-chain-hashes]
+                   #:when (hash-has-key? dom-chain-hash entry))
+          (let ([dom-chain (hash-ref dom-chain-hash entry)])
+            (if dom-chain-acc
+              (olset-intersect dom-chain-acc dom-chain)
+              dom-chain))))
+      (if dom-chain
+        (hash-set dom-chain-hashes-acc entry dom-chain)
+        dom-chain-hashes-acc)))
+
+  (define (init label-table)
+    (for/hash ([(label _) label-table])
+      (values label
+              (if (member label entries)
+                (hash label (list label))
+                (hash)))))
+
+  (define (update! builder)
+    (set! changed #f)
+    (for/hash ([label rpo])
+      (values label
+              (if (member label entries)
+                (hash-ref builder label)
+                (let ([dom-chain-hash (hash-ref builder label)]
+                      [dom-chain-hash*
+                       (intersect*
+                         (for/list ([label (hash-ref (hash-ref label-table label) 'callers)])
+                           (hash-ref builder label)))])
+                  (for ([(entry dom-chain*) dom-chain-hash*])
+                    (unless (and (hash-has-key? dom-chain-hash entry)
+                                 (olset= dom-chain* (hash-ref dom-chain-hash entry)))
+                      (set! changed #t)))
+                  dom-chain-hash*)))))
+
+  (define (children builder entry label)
+    (filter (λ (label*)
+              (define dom-chain-hash (hash-ref builder label*))
+              (and (hash-has-key? dom-chain-hash entry)
+                   (eq? (car (hash-ref dom-chain-hash entry))
+                        label)))
+            (drop rpo (+ (hash-ref label-rpo-indices label) 1))))
+
+  (define (build builder)
+    (for/hash ([entry entries])
+      (values entry
+              (let build-node ([label entry])
+                (cons label (map build-node (children builder entry label)))))))
+
+  (let iterate ([builder (init label-table)])
+    (if changed
+      (iterate (update! builder))
+      (build builder))))
 
 (require (prefix-in cc-ltab: (submod "." cpcps-label-table)))
 
