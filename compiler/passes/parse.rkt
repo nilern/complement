f32e45265ee74c25ceddf9ef8188ba50931af4c4
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2017-12-04 18:37:03 +0200

    Add infix operators.

--- compiler/passes/parse.rkt
+++ compiler/passes/parse.rkt
@@ -11,7 +11,7 @@
 (provide parse)
 
 (define-tokens payload-toks
-  (LEX DYN PRIMOP STRING
+  (LEX DYN OP1 OP2 OP3 OP4 OP5 OP6 OP7 PRIMOP STRING
    INT CHAR))
 (define-empty-tokens empty-toks
   (EOF
@@ -20,6 +20,10 @@
    = => \|))
 
 (define-lex-abbrevs
+  (delimiter (union #\( #\) #\[ #\] #\{ #\} #\" #\' #\`))
+  (separator (union #\, #\;))
+  (terminator (union delimiter separator whitespace))
+  (constituents (complement (:: any-string terminator any-string)))
   (lower-letter (:/ "a" "z"))
   (upper-letter (:/ #\A #\Z))
   (digit (:/ #\0 #\9)))
@@ -38,6 +42,22 @@
     ["=>" '=>]
     ["=" '=]
     ["|" '\|]
+    [(:: #\| constituents)
+     (token-OP1 (string->symbol lexeme))]
+    [(:: #\^ constituents)
+     (token-OP2 (string->symbol lexeme))]
+    [(:: #\& constituents)
+     (token-OP3 (string->symbol lexeme))]
+    [(:: (union #\= #\!) constituents)
+     (token-OP4 (string->symbol lexeme))]
+    [(:: (union #\= #\!) constituents)
+     (token-OP4 (string->symbol lexeme))]
+    [(:: (union #\< #\>) constituents)
+     (token-OP5 (string->symbol lexeme))]
+    [(:: (union #\+ #\-) constituents)
+     (token-OP6 (string->symbol lexeme))]
+    [(:: (union #\* #\/ #\%) constituents)
+     (token-OP7 (string->symbol lexeme))]
     [(:: "\"" (:* (complement (:: any-string "\"" any-string))) "\"")
      (token-STRING (substring lexeme 1 (- (string-length lexeme) 1)))]
     [(:: "'" (complement (:: any-string "'" any-string)) "'")
@@ -119,7 +139,31 @@
         [(var = expr) ($def $1 $3)]
         [(expr) $1])
 
-      (expr
+      (expr [(infix1) $1])
+
+      (infix1
+        [(infix1 OP1 infix2) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(infix2) $1])
+      (infix2
+        [(infix2 OP2 infix3) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(infix3) $1])
+      (infix3
+        [(infix3 OP3 infix4) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(infix4) $1])
+      (infix4
+        [(infix4 OP4 infix5) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(infix5) $1])
+      (infix5
+        [(infix5 OP5 infix6) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(infix6) $1])
+      (infix6
+        [(infix6 OP6 infix7) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(infix7) $1])
+      (infix7
+        [(infix7 OP7 app-expr) (with-output-language (Cst Expr) `(call (lex ,$2) ,$1 ,$3))]
+        [(app-expr) $1])
+
+      (app-expr
         [(app) (match (reverse $1)
                  [(list expr) expr]
                  [(cons f args) (with-output-language (Cst Expr)
@@ -142,8 +186,15 @@
         [(datum) $1])
 
       (var
-        [(LEX) (with-output-language (Cst Var) `(lex ,$1))]
-        [(DYN) (with-output-language (Cst Var) `(dyn ,$1))])
+        [(LEX)               (with-output-language (Cst Var) `(lex ,$1))]
+        [(DYN)               (with-output-language (Cst Var) `(dyn ,$1))]
+        [(LPAREN OP1 RPAREN) (with-output-language (Cst Var) `(lex ,$2))]
+        [(LPAREN OP2 RPAREN) (with-output-language (Cst Var) `(lex ,$2))]
+        [(LPAREN OP3 RPAREN) (with-output-language (Cst Var) `(lex ,$2))]
+        [(LPAREN OP4 RPAREN) (with-output-language (Cst Var) `(lex ,$2))]
+        [(LPAREN OP5 RPAREN) (with-output-language (Cst Var) `(lex ,$2))]
+        [(LPAREN OP6 RPAREN) (with-output-language (Cst Var) `(lex ,$2))]
+        [(LPAREN OP7 RPAREN) (with-output-language (Cst Var) `(lex ,$2))])
 
       (datum
         [(datom) (with-output-language (Cst Expr) `(const ,$1))])
