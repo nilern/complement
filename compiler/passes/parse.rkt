4da4296e45a5970f77a1ad3f58fae410fbda4e9a
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2018-04-18 16:46:24 +0300

    Simplify parsing by only allowing expr:s as method bodies.

--- compiler/passes/parse.rkt
+++ compiler/passes/parse.rkt
@@ -83,61 +83,21 @@
 
 ;;;; # Parser
 
-;;; First, some hacks around LR limitations:
-
-(struct $fn-header (params cond body-start))
-(struct $def (var expr))
-
 (define (app->var-list app)
   (define (unwrap-var-expr expr)
     (nanopass-case (Cst Expr) expr
       [,x x]))
   (map unwrap-var-expr app))
 
-(define (parse-decls decls)
-  (define (make-case params cond body)
-    (with-output-language (Cst Case)
-      (if (list? params)
-        `(case (,params ...) ,cond ,body)
-        `(case ,params ,cond ,body))))
-
-  (if ($fn-header? (car decls))
-    (let loop ([decls decls]
-               [paramss '()]
-               [conds '()]
-               [bodies '()])
-      (match decls
-        [(cons ($fn-header params cond stmt) decls)
-         (let*-values ([(stmts decls) (splitf-at decls (negate $fn-header?))])
-           (loop decls
-                 (cons params paramss)
-                 (cons cond conds)
-                 (cons (extract-block (cons stmt stmts)) bodies)))]
-        ['() (with-output-language (Cst Expr)
-               `(fn ,(reverse (map make-case paramss conds bodies)) ...))]))
-    (extract-block decls)))
-
-(define (extract-block decls)
-  (let-values ([(stmts expr)
-                (let extract ([decls decls])
-                  (match decls
-                    [(list (and (not (? $fn-header?) (? $def?)) expr))
-                     (values '() expr)]
-                    [(cons ($def var expr) decls)
-                     (define stmt (with-output-language (Cst Stmt) `(def ,var ,expr)))
-                     (define-values (stmts expr*) (extract decls))
-                     (values (cons stmt stmts) expr*)]
-                    [(cons (and (not (? $fn-header?)) stmt) decls)
-                     (define-values (stmts expr) (extract decls))
-                     (values (cons stmt stmts) expr)]))])
-    (with-output-language (Cst Expr)
-      `(block ,stmts ... ,expr))))
-
-;;; The actual LALR parser (and some helpers):
-
 (define (binapp l op r)
   (with-output-language (Cst Expr)
     `(call (lex ,op) ,l ,r)))
+
+(define (make-case params cond body)
+  (with-output-language (Cst Case)
+    (if (list? params)
+      `(case (,params ...) ,cond ,body)
+      `(case ,params ,cond ,body))))
 
 (define parse-expr
   (parser
@@ -154,15 +114,17 @@
         [(body) $1])
 
       (body
-        [(stmt-list SEMICOLON expr) (extract-block (foldl cons (list $3) $1))]
-        [(expr) (extract-block (list $1))])
+        [(stmt-list SEMICOLON expr)
+         (with-output-language (Cst Expr)
+           `(block ,(reverse $1) ... ,$3))]
+        [(expr) $1])
 
       (stmt-list
         [(stmt-list SEMICOLON stmt) (cons $3 $1)]
         [(stmt) (list $1)])
 
       (stmt
-        [(var = expr) ($def $1 $3)]
+        [(var = expr) (with-output-language (Cst Stmt) `(def ,$1 ,$3))]
         [(expr) $1])
 
       (expr [(infix1) $1])
@@ -218,24 +180,23 @@
 
       (simple
         [(LPAREN expr RPAREN) $2]
-        [(LBRACE block RBRACE) $2]
+        [(LBRACE method-list RBRACE)
+         (with-output-language (Cst Expr)
+           `(fn ,(reverse $2) ...))]
+        [(LBRACE body RBRACE) $2]
         [(LBRACKET body RBRACKET)
          (with-output-language (Cst Expr)
            `(fn (case () (const #t) ,$2)))]
         [(var) $1]
         [(datum) $1])
 
-      (block
-        [(block-decl-list) (parse-decls (reverse $1))])
+      (method-list
+        [(method-list SEMICOLON method) (cons $3 $1)]
+        [(method) (list $1)])
 
-      (block-decl-list
-        [(block-decl) (list $1)]
-        [(block-decl-list SEMICOLON block-decl) (cons $3 $1)])
-
-      (block-decl
-        [(params => stmt) ($fn-header $1 (with-output-language (Cst Expr) `(const #t)) $3)]
-        [(params \| expr => stmt) ($fn-header $1 $3 $5)]
-        [(stmt) $1])
+      (method
+        [(params => expr) (make-case $1 (with-output-language (Cst Expr) `(const #t)) $3)]
+        [(params \| expr => expr) (make-case $1 $3 $5)])
 
       (params
         [(app)   (app->var-list (reverse $1))]
